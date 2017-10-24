a96e5ad9cd466f327f0577dc50a9c0a41c878ab8
HarpersGeekly <HarperRyanC@gmail.com>
2017-10-24 15:02:40 -0500

    Hashed password in RegisterServlet

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -1,6 +1,8 @@
 package com.codeup.adlister.controllers;
 import com.codeup.adlister.dao.DaoFactory;
 import com.codeup.adlister.models.User;
+import com.codeup.adlister.util.Password;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +13,7 @@ import java.io.IOException;
 @WebServlet(name = "controllers.RegisterServlet", urlPatterns = "/register")
 public class RegisterServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO: show the registration form
+        // show the registration form
         request.getRequestDispatcher("/WEB-INF/users/register.jsp").forward(request, response);
     }
 
@@ -20,7 +22,9 @@ public class RegisterServlet extends HttpServlet {
         String password = request.getParameter("password");
         String email = request.getParameter("email");
 
-        // TODO: ensure the submitted information is valid
+        String hashedPassword = Password.hash(password);
+
+        // ensure the submitted information is valid
         if (username.isEmpty() || password.isEmpty() || email.isEmpty()) {
             doGet(request, response); // show the register form again
         } else {
@@ -31,13 +35,13 @@ public class RegisterServlet extends HttpServlet {
                 return;
             }
 
-            // TODO: create a new user based off of the submitted information
+            // create a new user based off of the submitted information
             // a new user will have a 0 id..
-            User user = new User(0, username, email, password);
+            User user = new User(0, username, email, hashedPassword);
             // but we can set the id now for a newly inserted user.
             Long id = DaoFactory.getUsersDao().insert(user);
             user.setId(id);
-            // TODO: if a user was successfully created, send them to their profile
+            // if a user was successfully created, send them to their profile
             //start a new session for the newly registered user
             request.getSession().setAttribute("user", user);
             response.sendRedirect("/profile");

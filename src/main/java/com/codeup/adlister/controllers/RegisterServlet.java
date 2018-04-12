812a1f644e00c26dd69a26636e7029a08ed1da0b
HarpersGeekly <HarperRyanC@gmail.com>
2018-04-12 12:02:24 -0500

    Form validation in RegisterServlet. Mulitple if statements to check if fields are empty, if passwords don't match or length is correct, if email is valid. Each will display a message that is added to an ArrayList that has an attribute for the view.

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -1,6 +1,8 @@
 package com.codeup.adlister.controllers;
 import com.codeup.adlister.dao.DaoFactory;
 import com.codeup.adlister.models.User;
+import com.codeup.adlister.util.Email;
+import com.codeup.adlister.util.Password;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.Instant;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Date;
 
 @WebServlet(name = "controllers.RegisterServlet", urlPatterns = "/register")
@@ -25,15 +28,67 @@ public class RegisterServlet extends HttpServlet {
         String email = request.getParameter("email");
         String passwordConfirmation = request.getParameter("confirm_password");
 
-//        String hashedPassword = Password.hash(password);
+//        String hashedPassword = Password.hash(password); Don't have to hash here. User model's constructor takes care of that with setPassword();
 
         // ensure the submitted information is valid
-        boolean inputHasErrors = username.isEmpty()
-                || email.isEmpty()
-                || password.isEmpty()
-                || (!password.equals(passwordConfirmation));
+//        boolean inputHasErrors = username.isEmpty()
+//                || email.isEmpty()
+//                || password.isEmpty()
+//                || (!password.equals(passwordConfirmation));
+        boolean inputHasErrors = false;
+
+        boolean usernameEmpty = username.isEmpty();
+        boolean passwordEmpty = password.isEmpty();
+        boolean emailEmpty = email.isEmpty();
+        boolean passwordsNotMatch = !password.equals(passwordConfirmation);
+        User userExists = DaoFactory.getUsersDao().findByUsername(username);
+
+        ArrayList<String> listOfErrors = new ArrayList<>();
+
+        if (usernameEmpty) {
+            String usernameIsEmpty = "You must enter an username.";
+            listOfErrors.add(usernameIsEmpty);
+            inputHasErrors = true;
+        }
+
+        if (emailEmpty) {
+            String emailIsEmpty = "You must enter a valid email.";
+            listOfErrors.add(emailIsEmpty);
+            inputHasErrors = true;
+        }
+
+        if (passwordEmpty) {
+            String passwordIsEmpty = "You must enter a password.";
+            listOfErrors.add(passwordIsEmpty);
+            inputHasErrors = true;
+        }
+
+        if (passwordsNotMatch) {
+            String passwordNotMatch = "Your passwords don't match.";
+            listOfErrors.add(passwordNotMatch);
+            inputHasErrors = true;
+        }
+
+        if(!Email.validate(email) && !emailEmpty) {
+            String invalidEmail = "You have entered an invalid email format.";
+            listOfErrors.add(invalidEmail);
+            inputHasErrors = true;
+        }
+
+        if(!Password.validateLength(password)) {
+            String invalidPassword = "Password must be minimum 4 characters long.";
+            listOfErrors.add(invalidPassword);
+            inputHasErrors = true;
+        }
+
+        if(userExists != null) {
+            String usernameExists = "A username with that name already exists. Please try another username.";
+            listOfErrors.add(usernameExists);
+            inputHasErrors = true;
+        }
 
         if (inputHasErrors) {
+            request.getSession().setAttribute("listOfErrors", listOfErrors);
             doGet(request, response); // show the register form again
 
         } else {
